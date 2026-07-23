04442851d64c97dc12bfb79df217582a4565c9c6
Modi Preyal <modipreyal@gmail.com>
2026-07-23 20:14:57 +0530

    Time: 0 ms (100.00%) | Memory: 32 MB (76.27%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.py
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.py
@@ -1,6 +1,21 @@
+# time complexity: O(1)
+# space complexity: O(1)
+import math
+from typing import List
+
+
 class Solution:
-  def uniqueXorTriplets(self, nums: list[int]) -> int:
-    n = len(nums)
-    if n < 3:
-      return n
-    return 1 << (int(math.log2(n)) + 1)
+    def uniqueXorTriplets(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        b = int(math.floor(math.log2(n))) + 1
+        return 1 << b
+
+
+nums = [1, 2]
+print(Solution().uniqueXorTriplets(nums))
+nums = [3, 1, 2]
+print(Solution().uniqueXorTriplets(nums))
