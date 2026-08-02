18f5eb5ee01575289d4ee01229b5a31124c77355
Modi Preyal <modipreyal@gmail.com>
2026-08-02 18:16:00 +0530

    Time: 119 ms (42.71%) | Memory: 25.5 MB (33.37%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,9 +1,16 @@
 class Solution:
-    def stoneGame(self, piles: List[int]) -> bool:
-        @cache
-        def dfs(i: int, j: int) -> int:
-            if i > j:
-                return 0
-            return max(piles[i] - dfs(i + 1, j), piles[j] - dfs(i, j - 1))
+  def stoneGame(self, piles: list[int]) -> bool:
+    n = len(piles)
+    # dp[i][j] := the maximum stones you can get more than your opponent in piles[i..j]
+    dp = [[0] * n for _ in range(n)]
 
-        return dfs(0, len(piles) - 1) > 0
+    for i, pile in enumerate(piles):
+      dp[i][i] = pile
+
+    for d in range(1, n):
+      for i in range(n - d):
+        j = i + d
+        dp[i][j] = max(piles[i] - dp[i + 1][j],
+                       piles[j] - dp[i][j - 1])
+
+    return dp[0][n - 1] > 0
