e6a01ba896a0b20e120c999aca257b3c7118bdd1
Modi Preyal <modipreyal@gmail.com>
2026-08-03 18:59:41 +0530

    Time: 1226 ms (34.26%) | Memory: 227.5 MB (13.80%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.py
+++ 1522-stone-game-iii/stone-game-iii.py
@@ -1,25 +1,19 @@
 class Solution:
-  def stoneGameIII(self, stoneValue: list[int]) -> str:
-    @functools.lru_cache(None)
-    def dp(i: int) -> int:
-      """
-      Returns the maximum relative score Alice can make with stoneValue[i..n).
-      """
-      if i == len(stoneValue):
-        return 0
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        @cache
+        def dfs(i: int) -> int:
+            if i >= len(stoneValue):
+                return 0
+            ans = -inf
+            s = 0
+            for j in range(i, i + 3):
+                if j >= len(stoneValue):
+                    break
+                s += stoneValue[j]
+                ans = max(ans, s - dfs(j + 1))
+            return ans
 
-      res = -math.inf
-      summ = 0
-
-      for j in range(i, i + 3):
-        if j == len(stoneValue):
-          break
-        summ += stoneValue[j]
-        res = max(res, summ - dp(j + 1))
-
-      return res
-
-    score = dp(0)
-    if score == 0:
-      return 'Tie'
-    return 'Alice' if score > 0 else 'Bob'
+        res = dfs(0)
+        if res == 0:
+            return 'Tie'
+        return 'Alice' if res > 0 else 'Bob'
