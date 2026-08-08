763cd523b32130a177117d25a080c1d805e8b26e
Modi Preyal <modipreyal@gmail.com>
2026-08-08 17:42:08 +0530

    Time: 478 ms (65.22%) | Memory: 47.1 MB (71.74%) - LeetSync

--- 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.py
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.py
@@ -1,29 +1,34 @@
 class Solution:
-  def validSequence(self, word1: str, word2: str) -> list[int]:
-    ans = []
-    # last[j] := the index i of the last occurrence in word1, where
-    # word1[i] == word2[j]
-    last = [-1] * len(word2)
+    def validSequence(self, word1: str, word2: str) -> list[int]:
+        n, m = len(word1), len(word2)
 
-    i = len(word1) - 1
-    j = len(word2) - 1
-    while i >= 0 and j >= 0:
-      if word1[i] == word2[j]:
-        last[j] = i
-        j -= 1
-      i -= 1
+        # last[j] = latest index in word1 where word2[j:] can start matching
+        last = [-1] * m
 
-    canSkip = True
-    j = 0
-    for i, c in enumerate(word1):
-      if j == len(word2):
-        break
-      if c == word2[j]:
-        ans.append(i)
-        j += 1
-      elif canSkip and (j == len(word2) - 1 or i < last[j + 1]):
-        canSkip = False
-        ans.append(i)
-        j += 1
+        i = n - 1
+        j = m - 1
 
-    return ans if j == len(word2) else []
+        while i >= 0 and j >= 0:
+            if word1[i] == word2[j]:
+                last[j] = i
+                j -= 1
+            i -= 1
+
+        ans = []
+        j = 0
+        canSkip = True
+
+        for i, c in enumerate(word1):
+            if j == m:
+                break
+
+            if c == word2[j]:
+                ans.append(i)
+                j += 1
+
+            elif canSkip and (j == m - 1 or i < last[j + 1]):
+                ans.append(i)
+                j += 1
+                canSkip = False
+
+        return ans if j == m else []
