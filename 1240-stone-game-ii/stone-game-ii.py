3b91b16a6ec65dcc460d440e9c1cd4e51a29034e
Modi Preyal <modipreyal@gmail.com>
2026-08-09 18:32:40 +0530

    Time: 69 ms (90.87%) | Memory: 26 MB (57.27%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.py
+++ 1240-stone-game-ii/stone-game-ii.py
@@ -1,13 +1,28 @@
+from functools import cache
+from typing import List
+
 class Solution:
     def stoneGameII(self, piles: List[int]) -> int:
+        n = len(piles)
+        suffix = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+            suffix[i] = suffix[i + 1] + piles[i]
+
         @cache
         def dfs(i, m):
-            if m * 2 >= n - i:
-                return s[n] - s[i]
-            return max(
-                s[n] - s[i] - dfs(i + x, max(m, x)) for x in range(1, m << 1 | 1)
-            )
+            if (m << 1) >= n - i:
+                return suffix[i]
 
-        n = len(piles)
-        s = list(accumulate(piles, initial=0))
+            total = suffix[i]
+            best = 0
+
+            for x in range(1, (m << 1) + 1):
+                current = total - dfs(i + x, max(m, x))
+
+                if current > best:
+                    best = current
+
+            return best
+
         return dfs(0, 1)
