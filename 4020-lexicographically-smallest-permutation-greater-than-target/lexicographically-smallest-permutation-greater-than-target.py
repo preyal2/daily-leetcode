3811bcedcc11ae393e23bbb18004e71f2c130f1d
Modi Preyal <modipreyal@gmail.com>
2026-08-27 18:06:47 +0530

    Time: 7 ms (88.16%) | Memory: 19.3 MB (71.05%) - LeetSync

--- 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.py
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.py
@@ -1,61 +1,48 @@
 class Solution:
     def lexGreaterPermutation(self, s: str, target: str) -> str:
-        n = len(s)
-
-        # Frequency of characters in s
         freq = [0] * 26
+
         for c in s:
             freq[ord(c) - 97] += 1
 
-        # Prefix frequency of target.
-        # Initially contains all target characters.
-        pref = [0] * 26
-        for c in target:
-            pref[ord(c) - 97] += 1
+        rem = freq[:]
+        best_i = -1
+        best_c = -1
 
-        for i in range(n - 1, -1, -1):
-            ti = ord(target[i]) - 97
+        # Find the rightmost position where target can be made
+        # and we can replace target[i] with a larger character.
+        for i, c in enumerate(target):
+            x = ord(c) - 97
 
-            # pref now represents target[:i]
-            pref[ti] -= 1
-
-            # target[:i] must be constructible from s
-            possible = True
-            for x in range(26):
-                if pref[x] > freq[x]:
-                    possible = False
+            # Try to make this position strictly larger.
+            for y in range(x + 1, 26):
+                if rem[y]:
+                    best_i = i
+                    best_c = y
                     break
 
-            if not possible:
-                continue
+            # Use target[i] for continuing the equal prefix.
+            rem[x] -= 1
 
-            # Find the smallest available character > target[i]
-            bigger = -1
-            for x in range(ti + 1, 26):
-                if freq[x] > pref[x]:
-                    bigger = x
-                    break
+            # target prefix is impossible from this point onward.
+            if rem[x] < 0:
+                break
 
-            if bigger == -1:
-                continue
+        if best_i == -1:
+            return ""
 
-            # Build answer:
-            # target[:i] + bigger + smallest possible suffix
-            ans = list(target[:i])
-            ans.append(chr(bigger + 97))
+        # Rebuild counts for the best position.
+        rem = freq[:]
+        for i in range(best_i):
+            rem[ord(target[i]) - 97] -= 1
 
-            # Remove prefix and chosen bigger character
-            remain = freq[:]
-            for x in range(26):
-                remain[x] -= pref[x]
+        rem[best_c] -= 1
 
-            remain[bigger] -= 1
+        # Smallest possible suffix.
+        ans = target[:best_i] + chr(best_c + 97)
 
-            # Smallest lexicographical suffix
-            for x in range(26):
-                if remain[x]:
-                    ans.append(chr(x + 97) * remain[x])
+        for x in range(26):
+            if rem[x]:
+                ans += chr(x + 97) * rem[x]
 
-            return ''.join(ans)
-
-        return ""
+        return ans
