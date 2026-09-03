0b95518409c3ad40cfceb3de336a8b4d10082307
Modi Preyal <modipreyal@gmail.com>
2026-09-03 17:53:23 +0530

    Time: 60 ms (62.19%) | Memory: 35.3 MB (51.83%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -1,4 +1,12 @@
 class Solution:
     def uniformArray(self, nums1: List[int]) -> bool:
-        mn = min((x for x in nums1 if x & 1), default=None)
-        return mn is None or all(x >= mn for x in nums1 if x & 1 == 0)
+        mn = 1 << 60
+
+        for x in nums1:
+            if x & 1 and x < mn:
+                mn = x
+
+        if mn == 1 << 60:
+            return True
+
+        return all(x >= mn for x in nums1 if not (x & 1))
