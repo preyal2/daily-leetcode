3ad0749604f87420781a10a1b5ee51d216c955ca
Modi Preyal <modipreyal@gmail.com>
2026-09-03 17:54:24 +0530

    Time: 54 ms (64.63%) | Memory: 35.3 MB (73.17%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -1,12 +1,16 @@
 class Solution:
     def uniformArray(self, nums1: List[int]) -> bool:
-        mn = 1 << 60
+        mn = float('inf')
 
         for x in nums1:
             if x & 1 and x < mn:
                 mn = x
 
-        if mn == 1 << 60:
+        if mn == float('inf'):
             return True
 
-        return all(x >= mn for x in nums1 if not (x & 1))
+        for x in nums1:
+            if not (x & 1) and x < mn:
+                return False
+
+        return True
