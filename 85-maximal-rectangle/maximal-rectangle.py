1a699a3d361074556840885f30a9a3d8b6386b1c
Modi Preyal <modipreyal@gmail.com>
2026-09-24 18:35:05 +0530

    Time: 35 ms (71.51%) | Memory: 24.5 MB (52.23%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.py
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -2,32 +2,35 @@ class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         heights = [0] * len(matrix[0])
         ans = 0
+
         for row in matrix:
-            for j, v in enumerate(row):
-                if v == "1":
+            for j in range(len(row)):
+                if row[j] == "1":
                     heights[j] += 1
                 else:
                     heights[j] = 0
-            ans = max(ans, self.largestRectangleArea(heights))
+
+            cur = self.largestRectangleArea(heights)
+            if cur > ans:
+                ans = cur
+
         return ans
 
     def largestRectangleArea(self, heights: List[int]) -> int:
-        n = len(heights)
-        stk = []
-        left = [-1] * n
-        right = [n] * n
-        for i, h in enumerate(heights):
-            while stk and heights[stk[-1]] >= h:
-                stk.pop()
-            if stk:
-                left[i] = stk[-1]
-            stk.append(i)
-        stk = []
-        for i in range(n - 1, -1, -1):
-            h = heights[i]
-            while stk and heights[stk[-1]] >= h:
-                stk.pop()
-            if stk:
-                right[i] = stk[-1]
-            stk.append(i)
-        return max(h * (right[i] - left[i] - 1) for i, h in enumerate(heights))
+        stack = []
+        ans = 0
+
+        for i in range(len(heights) + 1):
+            h = heights[i] if i < len(heights) else 0
+
+            while stack and heights[stack[-1]] > h:
+                height = heights[stack.pop()]
+                left = stack[-1] + 1 if stack else 0
+                area = height * (i - left)
+
+                if area > ans:
+                    ans = area
+
+            stack.append(i)
+
+        return ans
